ef66c4a066e3db0850e8a3b07a810abee688c7cf
inf-glitch <me@example.com>
2026-03-22 18:25:57 +0500

    fixed shell wrapper

--- rdi-vcs.sh
+++ rdi-vcs.sh
@@ -2,10 +2,9 @@
 ARGS="$@"
 set --
 
-# shellcheck source=/dev/null
+. ~/.env.sh
+
 . "${RDI_VCS_INSTALLATION}/miniforge3/bin/activate"
 conda activate rdi-vcs
 
-# TODO: add ssh key path passing
-
-python "$RDI_VCS_INSTALLATION/rdi_cli.py" "$ARGS"
+python $RDI_VCS_INSTALLATION/rdi_cli.py $ARGS
